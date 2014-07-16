e9bb2cd544e2cf7dddd57f462afd289b59cead1d
Hermanik <hermann@webvalley.cz>
2014-07-16 18:32:07 +0200

    font,color,text

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Hermanik on 10.07.14.
+ */
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2
+                },
+                files: {
+                    "src/css/style.css": "src/less/core.less"
+                }
+            }
+        },
+        watch: {
+            styles: {
+                files: ['src/less/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+};
