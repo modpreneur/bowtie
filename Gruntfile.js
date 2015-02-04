70943a0a31d267fafa70334495873870f6365109
Hermanik <hermann@webvalley.cz>
2015-02-04 14:17:55 +0100

    fixing sidebar with dropdown, harmonica, timeline, add spacing, add color venus&adonis

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,14 +5,33 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         less: {
-            development: {
+            responsive: {
                 options: {
                     compress: true,
                     yuicompress: true,
-                    optimization: 2
+                    optimization: 2,
+                    paths: ["less/"],
+                    modifyVars: {
+                        "columns" : 24
+                    }
                 },
                 files: {
                     "src/css/bowtie.css": "src/less/core.less"
+                }
+            },
+
+            noresponsive: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2,
+                    modifyVars: {
+                        "responsive" : false,
+                        "columns" : 24
+                    }
+                },
+                files: {
+                    "src/css/bowtie-noneresposive.css": "src/less/core.less"
                 }
             }
         },
@@ -30,5 +49,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('responsive', ['watch']);
+    grunt.registerTask('noresponsive', ['watch']);
 };
